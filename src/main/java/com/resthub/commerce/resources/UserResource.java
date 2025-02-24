628fb7b64bf7d37acaabe54c4d31fc59503c69ca
KellaoDev <keliofilho12@gmail.com>
2025-02-24 20:29:21 -0300

    User insert

--- src/main/java/com/resthub/commerce/resources/UserResource.java
+++ src/main/java/com/resthub/commerce/resources/UserResource.java
@@ -1,14 +1,14 @@
 package com.resthub.commerce.resources;
 
 import com.resthub.commerce.entities.User;
+import com.resthub.commerce.repositories.UserRepository;
 import com.resthub.commerce.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -17,6 +17,8 @@ public class UserResource {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
@@ -30,4 +32,11 @@ public class UserResource {
         return ResponseEntity.ok().body(obj);
     }
 
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = userRepository.save(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
 }
