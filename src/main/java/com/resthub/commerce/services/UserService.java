6aff94fabca982b7022ce366bd48a8f7262acf00
kelio <keliofilho12@gmail.com>
2025-02-25 13:29:34 -0300

    Exception handling - findById

--- src/main/java/com/resthub/commerce/services/UserService.java
+++ src/main/java/com/resthub/commerce/services/UserService.java
@@ -2,6 +2,7 @@ package com.resthub.commerce.services;
 
 import com.resthub.commerce.entities.User;
 import com.resthub.commerce.repositories.UserRepository;
+import com.resthub.commerce.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
     
     public User insert(User obj) {
