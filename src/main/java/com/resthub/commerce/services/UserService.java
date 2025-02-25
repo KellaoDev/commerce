1429f9029213fa3ac4dc47b0d8d3a68614830c70
kelio <keliofilho12@gmail.com>
2025-02-25 13:44:36 -0300

    Exception handling - update

--- src/main/java/com/resthub/commerce/services/UserService.java
+++ src/main/java/com/resthub/commerce/services/UserService.java
@@ -4,6 +4,7 @@ import com.resthub.commerce.entities.User;
 import com.resthub.commerce.repositories.UserRepository;
 import com.resthub.commerce.services.exceptions.DatabaseException;
 import com.resthub.commerce.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -42,9 +43,14 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, obj);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+
     }
 
     private void updateData(User entity, User obj) {
